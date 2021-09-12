dd9f751457dfac8f3cd8b62a7dc2caa53503cc8d
Eltoney <aliahmedeltoney@gmail.com>
2021-09-12 14:24:59 +0200

    lab 3 tasks

---
+++ CSharp/Lab3/arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            System.Console.WriteLine("Enter the array numbers one by one in seperated line");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0, mxVal = arr[0], mnVal = arr[0];
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > mxVal)
+                {
+                    mxVal = arr[i];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] < mnVal)
+                {
+                    mnVal = arr[i];
+                }
+            }
+            System.Console.WriteLine($"The sum of your array is {sum}");
+            System.Console.WriteLine($"The minumim number in the array is {mnVal}");
+            System.Console.WriteLine($"The maximum number in the array is {mxVal}");
+        }
+    }
+}
---
+++ CSharp/Lab3/methods/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static long factorial(int x)
+        {
+            long ans = 1;
+            for (int i = 1; i < x; i++)
+            {
+                ans *= i;
+            }
+            return ans;
+        }
+
+        static long calcPower(int b, int p)
+        {
+            long ans = 1;
+            for (int i = 0; i < p; i++)
+            {
+                ans *= b;
+            }
+            return ans;
+        }
+
+
+        static long calc(int x, int y, char op)
+        {
+            long ans = 0;
+            switch (op)
+            {
+                case '+':
+                    ans = x + y;
+                    break;
+                case '-':
+                    ans = x - y;
+                    break;
+                case '*':
+                    ans = x * y;
+                    break;
+                case '/':
+                    ans = x / y;
+                    break;
+                default:
+                    System.Console.WriteLine("You enterd a wrong operation");
+                    break;
+            }
+            return ans;
+        }
+        static void swapNumber(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = x;
+        }
+
+        static void stringAnalyser(string str)
+        {
+            string[] arr = str.Split(" ");
+            int wordCount = arr.Length, charCount = 0;
+            foreach (string word in arr)
+            {
+                charCount += word.Length;
+            }
+            System.Console.WriteLine($"The string has {wordCount} words and {charCount} characters");
+
+        }
+    }
+}
