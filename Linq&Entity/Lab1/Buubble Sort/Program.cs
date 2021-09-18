8a43382fb0a12b949aee0bb7d0d2840deacf3539
Eltoney <aliahmedeltoney@gmail.com>
2021-09-18 14:15:00 +0200

    New labs

---
+++ Linq&Entity/Lab1/Buubble Sort/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Buubble_Sort
+{
+    class bubSort
+    {
+        static bool Asc(int a, int b)
+        {
+            return a < b;
+        }
+
+        static bool Dsc(int a, int b)
+        {
+            return a > b;
+        }
+
+
+        delegate bool cmp(int a, int b);
+
+        void bubleSort(int[] arr, cmp compare)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (compare(arr[j], arr[j + 1]))
+                    {
+                        swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+        }
+
+        private void swap(ref int v1, ref int v2)
+        {
+            int temp = v1;
+            v1 = v2;
+            v2 = temp;
+        }
+
+        public void bubleSortAsc(int[] arr)
+        {
+
+            cmp ascSort = Asc;
+            bubleSort(arr, Asc);
+        }
+
+        public void bubleSortDsc(int[] arr)
+        {
+
+
+            cmp dscSort = Dsc;
+            bubleSort(arr, Dsc);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 4, 10, 1, 5, 3 };
+            bubSort b = new bubSort();
+            b.bubleSortAsc(arr);
+            System.Console.Write("Ascending  order is: ");
+            foreach (int x in arr)
+            {
+                System.Console.Write($"{x} ");
+            }
+            System.Console.WriteLine();
+
+            b.bubleSortDsc(arr);
+            System.Console.Write("Descending order is: ");
+            foreach (int x in arr)
+            {
+                System.Console.Write($"{x} ");
+            }
+             System.Console.WriteLine();
+        }
+    }
+}
---
+++ Linq&Entity/Lab1/company/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+
+
+namespace company
+{
+    class employee
+    {
+        int id, salary;
+        string name;
+
+        public int Id { set { id = value; } get { return id; } }
+        public int Salary { set { salary = value; } get { return salary; } }
+        public string Name { set { name = value; } get { return name; } }
+
+        public void Print()
+        {
+            System.Console.WriteLine($"{Name} with id {Id} has salary {Salary} pounds");
+        }
+    }
+    class sortByID : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            employee e1 = (employee)x;
+            employee e2 = (employee)y;
+            return e1.Id.CompareTo(e2.Id);
+        }
+    }
+
+
+    class sortBySalary : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            employee e1 = (employee)x;
+            employee e2 = (employee)y;
+            return e1.Salary.CompareTo(e2.Salary);
+        }
+    }
+
+    class sortByName : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            employee e1 = (employee)x;
+            employee e2 = (employee)y;
+            return e1.Name.CompareTo(e2.Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee[] emp = new employee[]
+            {
+                new employee{Id = 1 , Salary = 5000 , Name = "Ali"},
+                new employee{Id = 2 , Salary = 4000 , Name = "Emad"},
+                new employee{Id = 3 , Salary = 3000 , Name = "Zeyad"},
+                new employee{Id = 4 , Salary = 2000 , Name = "Omar"},
+                new employee{Id = 5 , Salary = 1000 , Name = "Hassan"},
+            };
+            IComparer cmpId = new sortByID();
+            Array.Sort(emp, cmpId);
+            System.Console.WriteLine("Sorting employess with id: ");
+            foreach (employee e in emp)
+            {
+                e.Print();
+            }
+            System.Console.WriteLine();
+
+
+
+            IComparer cmpSalary = new sortBySalary();
+            Array.Sort(emp, cmpSalary);
+            System.Console.WriteLine("Sorting employess with salary: ");
+            foreach (employee e in emp)
+            {
+                e.Print();
+            }
+            System.Console.WriteLine();
+
+
+            IComparer cmpName = new sortByName();
+            Array.Sort(emp, cmpName);
+            System.Console.WriteLine("Sorting employess with name: ");
+            foreach (employee e in emp)
+            {
+                e.Print();
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
